58876abb2e0a6a0b7d1a20f1a173ae3f8e7b9e05
matiaslewin <148098428+matiaslewin@users.noreply.github.com>
2024-01-09 19:31:11 +0000

    ready

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() {};
+document.querySelector("#excuse").innerHTML = excuseGenerator();
+
+function excuseGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const whoRandom = Math.floor(Math.random() * who.length);
+  const actionRandom = Math.floor(Math.random() * action.length);
+  const whatRandom = Math.floor(Math.random() * what.length);
+  const whenRandom = Math.floor(Math.random() * when.length);
+  return (
+    who[whoRandom] +
+    " " +
+    action[actionRandom] +
+    " " +
+    what[whatRandom] +
+    " " +
+    when[whenRandom]
+  );
+}
